dee2b55e3f804893c06711e56b092b4f2f4bc876
yesjjin99 <yesjjin99@naver.com>
2023-10-30 16:36:54 +0900

    feat: 구입 금액 입력 기능 추가

---
+++ src/main/java/lotto/utils/constant/Constant.java
@@ -0,0 +1,5 @@
+package lotto.utils.constant;
+
+public class Constant {
+    public static final int LOTTO_AMOUNT = 1000;
+}
---
+++ src/main/java/lotto/utils/constant/ErrorMessage.java
@@ -0,0 +1,5 @@
+package lotto.utils.constant;
+
+public class ErrorMessage {
+    public static final String AMOUNT_DIVIDE_EXCEPTION = "[ERROR] 로또 구입 금액은 1,000원 단위로 입력해야 합니다.";
+}
